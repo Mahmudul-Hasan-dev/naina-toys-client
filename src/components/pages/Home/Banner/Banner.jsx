2712deff0a1cebaa2bf1aaaaffc3393365dd0520
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2024-05-24 17:11:31 +0600

    banner

---
+++ src/components/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Banner.css'
+import 'animate.css';
+const Banner = () => {
+    return (
+        <div>
+            <div className="banner">
+  
+                 <h1 className='banner-title py-5 ps-5 animate__animated  animate__backInLeft'>
+                    Pick the best toy 
+                    <br />for your kid
+                 </h1>
+                 <h2 className='banner-text ps-5 '>We make your children happier with the best toys</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
